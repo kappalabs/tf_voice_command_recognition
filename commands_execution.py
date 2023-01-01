2e1215f8a5254c4aaf240d595b2b5c260f93d109
Vojtěch Vašek <vojtech.vasek@matfyz.cz>
2023-01-01 21:01:05 +0100

    [FEATURE] Recording
     - implements arguments for the commands recording

--- commands_execution.py
+++ commands_execution.py
@@ -11,13 +11,21 @@ class CommandsExecution:
     def process_sentence(sentence: Tuple[Command, Command]):
         print("processing sentence", sentence[0].name, sentence[1].name)
 
-        if sentence[0].name == "go" and sentence[1].name == "on":
-            print("go on")
-            os.system('mosquitto_pub -t \'zigbee2mqtt/svetlo_chodba/set\' -m \'{"brightness": 254}\'')
-            os.system('mosquitto_pub -t \'zigbee2mqtt/svetlo_chodba_2/set\' -m \'{"brightness": 254}\'')
-        elif sentence[0].name == "go" and sentence[1].name == "off":
-            print("go off")
-            os.system('mosquitto_pub -t \'zigbee2mqtt/svetlo_chodba/set\' -m \'{"brightness": 0}\'')
-            os.system('mosquitto_pub -t \'zigbee2mqtt/svetlo_chodba_2/set\' -m \'{"brightness": 0}\'')
+        if sentence[0].name == "rozsvit" and sentence[1].name == "chodba":
+            os.system('mosquitto_pub -t \'zigbee2mqtt/svetlo_chodba_1/set\' -m \'{"state": "on"}\'')
+            os.system('mosquitto_pub -t \'zigbee2mqtt/svetlo_chodba_2/set\' -m \'{"state": "on"}\'')
+        elif sentence[0].name == "zhasni" and sentence[1].name == "chodba":
+            os.system('mosquitto_pub -t \'zigbee2mqtt/svetlo_chodba_1/set\' -m \'{"state": "off"}\'')
+            os.system('mosquitto_pub -t \'zigbee2mqtt/svetlo_chodba_2/set\' -m \'{"state": "off"}\'')
+        elif sentence[0].name == "rozsvit" and sentence[1].name == "pokoj":
+            os.system('mosquitto_pub -t \'zigbee2mqtt/svetlo_pokoj_1/set\' -m \'{"state": "on"}\'')
+            os.system('mosquitto_pub -t \'zigbee2mqtt/svetlo_pokoj_2/set\' -m \'{"state": "on"}\'')
+            os.system('mosquitto_pub -t \'zigbee2mqtt/svetlo_pokoj_3/set\' -m \'{"state": "on"}\'')
+            os.system('mosquitto_pub -t \'zigbee2mqtt/svetlo_pokoj_4/set\' -m \'{"state": "on"}\'')
+        elif sentence[0].name == "zhasni" and sentence[1].name == "pokoj":
+            os.system('mosquitto_pub -t \'zigbee2mqtt/svetlo_pokoj_1/set\' -m \'{"state": "off"}\'')
+            os.system('mosquitto_pub -t \'zigbee2mqtt/svetlo_pokoj_2/set\' -m \'{"state": "off"}\'')
+            os.system('mosquitto_pub -t \'zigbee2mqtt/svetlo_pokoj_3/set\' -m \'{"state": "off"}\'')
+            os.system('mosquitto_pub -t \'zigbee2mqtt/svetlo_pokoj_4/set\' -m \'{"state": "off"}\'')
         else:
             print("unknown sentence", sentence[0].name, sentence[1].name)
