d39c28511978baca08accaca3c3a7c0a50e31121
Vojtěch Vašek <vojtech.vasek@matfyz.cz>
2023-01-01 08:51:59 +0100

    [FEATURE] Go Online
     - implements online recognition

--- run_online.py
+++ run_online.py
@@ -1,20 +1,23 @@
+import io
 import os
 import glob
 import json
+import struct
 import random
-# import librosa
+import pyaudio
 
 import numpy as np
-# import tensorflow as tf
 import scipy.signal as sps
 
 from scipy.io import wavfile
 from tflite_support import metadata
 from tflite_runtime.interpreter import Interpreter
 
+# import tensorflow as tf
+
 TFLITE_FILENAME = 'browserfft-speech.tflite'
 SAVE_PATH = './models'
-test_dir = './dataset-test'
+# test_dir = './dataset-test'
 
 
 def get_random_audio_file(samples_dir):
@@ -43,6 +46,10 @@ def get_input_sample_rate(model):
 
 
 def wav_to_floats(wave_bytes, num_samples=16000):
+    # Convert the WAV file to a stream
+    if type(wave_bytes) is str:
+        wave_bytes = io.BytesIO(wave_bytes)
+
     # Read file
     sampling_rate, data = wavfile.read(wave_bytes)
 
@@ -54,35 +61,90 @@ def wav_to_floats(wave_bytes, num_samples=16000):
     return z
 
 
+def bytes_to_floats(wave_bytes, num_samples=16000):
+    a = struct.unpack("%ih" % int(len(wave_bytes) / RESPEAKER_WIDTH), wave_bytes)
+    a = [float(val) / pow(2, 15) for val in a]
+    a = np.asarray(a, dtype=np.float32)
+
+    return a
+
+
 # Get a WAV file for inference and list of labels from the model
 tflite_file = os.path.join(SAVE_PATH, TFLITE_FILENAME)
 labels = get_labels(tflite_file)
-random_audio = get_random_audio_file(test_dir)
+# random_audio = get_random_audio_file(test_dir)
 
-# Ensure the audio sample fits the model input
-# interpreter = tf.lite.Interpreter(tflite_file)
-interpreter = Interpreter(tflite_file)
+
+# import tensorflow as tf
+RESPEAKER_RATE = 44100
+RESPEAKER_CHANNELS = 1
+RESPEAKER_WIDTH = 2  # Sample width in bytes
+
+RESPEAKER_INDEX = 1
+CHUNK = RESPEAKER_RATE
+
+p = pyaudio.PyAudio()
+
+stream = p.open(
+    rate=RESPEAKER_RATE,
+    format=p.get_format_from_width(RESPEAKER_WIDTH),
+    channels=RESPEAKER_CHANNELS,
+    input=True,
+    # input_device_index=RESPEAKER_INDEX,
+)
+
+# interpreter = tf.lite.Interpreter(model_path=tflite_file)
+interpreter = Interpreter(model_path=tflite_file)
+interpreter.allocate_tensors()
 input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
-input_size = input_details[0]['shape'][1]
 sample_rate = get_input_sample_rate(tflite_file)
-# audio_data, _ = librosa.load(random_audio, sr=sample_rate)
-audio_data = wav_to_floats(random_audio, num_samples=sample_rate)
-if len(audio_data) < input_size:
-    audio_data.resize(input_size)
-audio_data = np.expand_dims(audio_data[:input_size], axis=0)
+input_size = input_details[0]['shape'][1]
 
-# Run inference
-interpreter.allocate_tensors()
-interpreter.set_tensor(input_details[0]['index'], audio_data)
-interpreter.invoke()
-output_data = interpreter.get_tensor(output_details[0]['index'])
+buffer = np.zeros((sample_rate * 2,), dtype=np.float32)
+buffer_index = 0
+buffer_skip = int(sample_rate / 8)
 
-# Display prediction and ground truth
-top_index = np.argmax(output_data[0])
-label = labels[top_index]
-score = output_data[0][top_index]
-print('---prediction---')
-print(f'Class: {label}\nScore: {score}')
-print('----truth----')
-print(random_audio.split('/')[-2])
+# Continuously read the input stream and classify the audio
+while True:
+    # Read the input stream
+    data = stream.read(CHUNK)
+    # Convert to float32
+    data = bytes_to_floats(data, num_samples=sample_rate)
+    # data = np.frombuffer(data, dtype=np.int16).astype(np.float32)
+    # # Reshape to (1, 44100)
+    # data = data.reshape((1, -1))
+    if len(data) < input_size:
+        data.resize(input_size)
+
+    # Keep last two chunks of audio in the buffer
+    if buffer_index < len(buffer):
+        buffer[buffer_index:buffer_index + len(data)] = data
+        buffer_index += len(data)
+
+        # Skip the classification if buffer is not full
+        continue
+    else:
+        buffer[:sample_rate] = buffer[sample_rate:]
+        buffer[sample_rate:sample_rate + len(data)] = data
+
+    # Run the model on the buffer with sliding window
+    for i in range(0, input_size, buffer_skip):
+        input_tensor = buffer[i:i + input_size]
+        input_tensor = np.expand_dims(input_tensor, axis=0)
+        # Set the input tensor
+        interpreter.set_tensor(input_details[0]['index'], input_tensor)
+        # Run the inference
+        interpreter.invoke()
+        # Get the output tensor
+        output_data = interpreter.get_tensor(output_details[0]['index'])
+        # Get the index of the highest probability
+        index = np.argmax(output_data)
+
+        if index != 0:
+            # Print the label and confidence
+            print(labels[index], output_data[0][index])
+
+stream.stop_stream()
+stream.close()
+p.terminate()
