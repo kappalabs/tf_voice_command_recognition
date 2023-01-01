ddf17b902a967efea6efc9cfa9e5362ca582296b
Vojtěch Vašek <vojtech.vasek@matfyz.cz>
2023-01-01 15:12:51 +0100

    [FEATURE] Recording
     - implements arguments for the commands recording

--- record.py
+++ record.py
@@ -1,45 +1,80 @@
+import os
 import time
 import wave
 import pyaudio
+import argparse
 
 
-RESPEAKER_RATE = 44100
-RESPEAKER_CHANNELS = 1 
-RESPEAKER_WIDTH = 2
-# run getDeviceInfo.py to get index
-RESPEAKER_INDEX = 1  # refer to input device id
-CHUNK = 1024
-RECORD_SECONDS = 2
-WAVE_OUTPUT_FILENAME = "output_{}.wav".format(int(time.time()))
+def get_configuration():
+    default_config = {
+        "respeaker_rate": 44100,
+        "respeaker_channels": 1,
+        "respeaker_width": 2,
+        "chunk": 1024,
+        "record_seconds": 2,
+        "record_label": "background",
+        "record_filename": "record_{}.wav".format(int(time.time())),
+    }
 
-p = pyaudio.PyAudio()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--respeaker_rate', type=int, default=default_config['respeaker_rate'])
+    parser.add_argument('--respeaker_channels', type=int, default=default_config['respeaker_channels'])
+    parser.add_argument('--respeaker_width', type=int, default=default_config['respeaker_width'])
+    parser.add_argument('--chunk', type=int, default=default_config['chunk'])
+    parser.add_argument('--record_seconds', type=int, default=default_config['record_seconds'])
+    parser.add_argument('--record_label', type=str, default=default_config['record_label'])
+    parser.add_argument('--record_filename', type=str, default=default_config['record_filename'])
 
-stream = p.open(
-            rate=RESPEAKER_RATE,
-            format=p.get_format_from_width(RESPEAKER_WIDTH),
-            channels=RESPEAKER_CHANNELS,
-            input=True,
-            #input_device_index=RESPEAKER_INDEX,
-            )
+    args = parser.parse_args()
+    default_config.update(vars(args))
 
-print("* recording")
+    return default_config
 
-frames = []
 
-for i in range(0, int(RESPEAKER_RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    frames.append(data)
+def record(config):
+    p = pyaudio.PyAudio()
 
-print("* done recording")
+    # Prepare the stream for microphone input
+    stream = p.open(
+        rate=config['respeaker_rate'],
+        format=p.get_format_from_width(config['respeaker_width']),
+        channels=config['respeaker_channels'],
+        input=True,
+        #input_device_index=RESPEAKER_INDEX,
+    )
 
-stream.stop_stream()
-stream.close()
-p.terminate()
+    print("* recording")
 
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(RESPEAKER_CHANNELS)
-wf.setsampwidth(p.get_sample_size(p.get_format_from_width(RESPEAKER_WIDTH)))
-wf.setframerate(RESPEAKER_RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
+    # Record for a few seconds
+    frames = []
+    for i in range(0, int(config['respeaker_rate'] / config['chunk'] * config['record_seconds'])):
+        data = stream.read(config['chunk'])
+        frames.append(data)
 
+    print("* done recording")
+
+    # Stop recording
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+    # Save the recorded data as a WAV file
+    dirpath = os.path.join(os.getcwd(), 'mydata', config['record_label'])
+    if not os.path.exists(dirpath):
+        os.makedirs(dirpath)
+    filepath = os.path.join(dirpath, config['record_filename'])
+    wf = wave.open(filepath, 'wb')
+    wf.setnchannels(config['respeaker_channels'])
+    wf.setsampwidth(p.get_sample_size(p.get_format_from_width(config['respeaker_width'])))
+    wf.setframerate(config['respeaker_rate'])
+    wf.writeframes(b''.join(frames))
+    wf.close()
+
+
+def main():
+    default_config = get_configuration()
+    record(default_config)
+
+
+if __name__ == "__main__":
+    main()
