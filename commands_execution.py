6e13889deb0468ab4789f427a7af9b68be1c3d55
Vojtěch Vašek <vojtech.vasek@matfyz.cz>
2023-01-24 20:33:38 +0100

    [FEATURE] New Commnads
     - adds new place to control lights

--- commands_execution.py
+++ commands_execution.py
@@ -33,5 +33,9 @@ class CommandsExecution:
         elif sentence[0].name == "zhasni" and sentence[1].name == "postel":
             os.system('mosquitto_pub -t \'zigbee2mqtt/svetlo_1100_1/set\' -m \'{"state": "off"}\'')
             os.system('mosquitto_pub -t \'zigbee2mqtt/svetlo_1100_2/set\' -m \'{"state": "off"}\'')
+        elif sentence[0].name == "rozsvit" and sentence[1].name == "koupelna":
+            os.system('mosquitto_pub -t \'zigbee2mqtt/svetlo_koupelna/set\' -m \'{"state": "on"}\'')
+        elif sentence[0].name == "zhasni" and sentence[1].name == "koupelna":
+            os.system('mosquitto_pub -t \'zigbee2mqtt/svetlo_koupelna/set\' -m \'{"state": "off"}\'')
         else:
             print("unknown sentence", sentence[0].name, sentence[1].name)
